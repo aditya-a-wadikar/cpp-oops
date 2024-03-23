19968c102617f19a742b4a2c68789fc93ef4edd4
aditya-a-wadikar <aditya.a.wadikar@gmail.com>
2024-03-23 15:44:26 +0530

    Inheritance and it's types

---
+++ OOP-16.cpp
@@ -0,0 +1,24 @@
+/*
+Inheritance
+    - class can be derived from another class
+    - derived class can access the properties of base class
+    - thus, one can create hirarchical structor to code
+    - provide reuseability widely known as DRY (do not repeat yourself)
+
+Types
+    - single : single parent class with single child class
+    - multiple : multiple parent classes with single child class
+    - hirarchical : single parent class with multiple child class
+    - multilevel : chain
+    - hybrid : combination of multiple and multi-level inheritance
+*/
+
+#include <iostream>
+using namespace std;
+
+
+
+int main() {
+    
+    return 0;
+}
