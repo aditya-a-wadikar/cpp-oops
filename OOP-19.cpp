89d63cf1334b89f2efec86ae68f6b576a2620a1a
aditya-a-wadikar <aditya.a.wadikar@gmail.com>
2024-03-24 17:22:40 +0530

    protected inheritance basics and syntax

--- OOP-19.cpp
+++ OOP-19.cpp
@@ -2,10 +2,16 @@
 below code demos following,
     - protected access specifiers
 
+Need
+    - private members cannot be inherited
+    - public members are accessible outside class also
+    - protected members are kind of private but can be accessible using
+
 Protected access specifiers
     - protected and public members of base class become protected member of derived class
 
-Table of behaviour
+Table of behaviour for data members in different access specifiers
+
     |----------------------------------------------------------------------------------------|
     | Access Specifier | Public Inheritance | Protected Inheritance | Private Inheritance    |
     |------------------|--------------------|-----------------------|------------------------|
@@ -16,4 +22,29 @@ Table of behaviour
     | protected member | protected          | protected             | private                |
     |----------------------------------------------------------------------------------------|
 
+Notes
+    - for inheritance private member cannot be accessed in any case
+
 */
+
+#include <iostream>
+using namespace std;
+
+class Base{
+    private:
+        int a;
+        int b;
+    protected:
+    public:
+};
+
+class Derived : protected Base{
+    private:
+    protected:
+    public:
+};
+
+int main() {
+    
+    return 0;
+}
